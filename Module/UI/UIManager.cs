d96dd175122f71b9ca771492f69796ac72536dac
fujisheng <fujisheng@foxmail.com>
2021-04-08 20:17:57 +0800

    save

--- Module/UI/UIManager.cs
+++ Module/UI/UIManager.cs
@@ -2,40 +2,40 @@
 
 namespace Framework.ILR.Module.UI
 {
-    public class UIManager : ModuleBase, IUIManager
-    {
-        Queue<ICommand> commandQueue = new Queue<ICommand>();
+    //public class UIManager : ModuleBase, IUIManager
+    //{
+    //    Queue<ICommand> commandQueue = new Queue<ICommand>();
 
-        public override async void OnUpdate()
-        {
+    //    public override async void OnUpdate()
+    //    {
             
-        }
+    //    }
 
-        public async void Open<TView>(object args = null) where TView : IView
-        {
-            var context = Contexts.Create<TView>();
-            var view = await context.CreateView();
-        }
+    //    public async void Open<TView>(object args = null) where TView : IView
+    //    {
+    //        var context = Contexts.Create<TView>();
+    //        var view = await context.CreateView();
+    //    }
 
-        public async void Open<TView, TViewModel>(object args = null) where TView : IView where TViewModel : IViewModel
-        {
-            var context = Contexts.Create<TViewModel, TView>();
-            var view = await context.CreateView();
-        }
+    //    public async void Open<TView, TViewModel>(object args = null) where TView : IView where TViewModel : IViewModel
+    //    {
+    //        var context = Contexts.Create<TViewModel, TView>();
+    //        var view = await context.CreateView();
+    //    }
 
-        public void Open(string viewName, object args = null)
-        {
+    //    public void Open(string viewName, object args = null)
+    //    {
             
-        }
+    //    }
 
-        public void Close(string viewName, object args = null)
-        {
+    //    public void Close(string viewName, object args = null)
+    //    {
             
-        }
+    //    }
 
-        public void Back(int backCount = 1, object args = null)
-        {
+    //    public void Back(int backCount = 1, object args = null)
+    //    {
             
-        }
-    }
+    //    }
+    //}
 }
